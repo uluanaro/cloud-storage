ceaf2f0b060873ed058f3e9a8833b72f54a2959f
uluana_roshina <uluanasev15@gmail.com>
2026-06-27 21:34:50 +0300

    Add user auth: register, login, logout, home page

--- users/views.py
+++ users/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def register_view(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)  # заполняем форму данными из запроса
+        if form.is_valid():  # Django проверяет валидность
+            user = form.save()  # создаёт пользователя в БД
+            login(request, user)  # создаёт сессию в Redis
+            return redirect("/")  # редирект на главную
+    else:
+        form = UserCreationForm()  # пустая форма для GET запроса
+
+    return render(request, "users/register.html", {"form": form})
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("/")
+    else:
+        form = AuthenticationForm()
+    return render(request, "users/login.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect("/")
